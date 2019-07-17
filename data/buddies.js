b41bcfb3d96aabe64cb4b5a1bf0b0c619278f30c
Rina Razumov <rinash3@gmail.com>
2019-07-17 19:51:09 -0400

    created seeds file

---
+++ data/buddies.js
@@ -0,0 +1,85 @@
+var buddiesList = [{
+
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "email": "test@gmail.com",
+        "age": "27",
+        "country": "us",
+        "language": "English",
+        "gender": "Male",
+        "diet": "vegan",
+        "description": "i love travel",
+        "past_travel": "i have been to London and Paris",
+        "intrests": "nature,food",
+        "person": "funn person with same interests",
+        "dest1": "New York",
+        "dest2": "",
+        "dest3": "",
+        "must_see": "Times Square",
+        "prefered_trip": "planned"
+
+
+    },
+    {
+        "name": "Rose",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "email": "test1@gmail.com",
+        "age": "29",
+        "country": "Paris",
+        "language": "French",
+        "gender": "Female",
+        "diet": "vegan",
+        "description": "i love travel",
+        "past_travel": "i have been to London and Paris",
+        "intrests": "nature,food",
+        "person": "funn person with same interests",
+        "dest1": "New York",
+        "dest2": "",
+        "dest3": "",
+        "must_see": "Times Square",
+        "prefered_trip": "planned"
+
+
+    },
+    {
+        "name": "Ally",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "email": "test1@gmail.com",
+        "age": "29",
+        "country": "London",
+        "language": "English",
+        "gender": "Female",
+        "diet": "halal",
+        "description": "i love travel",
+        "past_travel": "i have been to London and Paris",
+        "intrests": "nature,food",
+        "person": "funn person with same interests",
+        "dest1": "New York",
+        "dest2": "",
+        "dest3": "",
+        "must_see": "Times Square",
+        "prefered_trip": "Spontaneous",
+    },
+    {
+        "name": "Don",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "email": "test1@gmail.com",
+        "age": "29",
+        "country": "Paris",
+        "language": "French",
+        "gender": "Male",
+        "diet": "vegan",
+        "description": "i love travel",
+        "past_travel": "i have been to London and Paris",
+        "intrests": "nature,food",
+        "person": "funn person with same interests",
+        "dest1": "New York",
+        "dest2": "",
+        "dest3": "",
+        "must_see": "Times Square",
+        "prefered_trip": "planned"
+
+    }
+]
+
+module.exports = buddiesList;
