91ac0351bc95c116c1e5379bc8b90a98f38ec35c
Rina Razumov <rinash3@gmail.com>
2019-07-18 19:58:51 -0400

    update friends file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,32 +1,38 @@
 var db = require("../models");
 var path = require("path");
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-      db.Buddies.findAll({}).then(function() {
-        res.sendFile(path.join(__dirname, "../public/html/index.html"));
-      });
+  app.get("/", function (req, res) {
+    db.Buddies.findAll({}).then(function () {
+      res.sendFile(path.join(__dirname, "../public/html/index.html"));
+    });
   });
 
-  app.get("/signup", function(req, res) {
-    db.Buddies.findAll({}).then(function() {
+  app.get("/signup", function (req, res) {
+    db.Buddies.findAll({}).then(function () {
       res.sendFile(path.join(__dirname, "../public/html/signup.html"));
     });
   });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Buddies.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
+  app.get("/display", function (req, res) {
+    db.Buddies.findAll({}).then(function (result) {
+      res.render("friendsdisplay", {
+        buddy: result
       });
     });
   });
+  // Load example page and pass in an example by id
+  // app.get("/example/:id", function(req, res) {
+  //   db.Buddies.findOne({ where: { id: req.params.id } }).then(function(
+  //     dbExample
+  //   ) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
